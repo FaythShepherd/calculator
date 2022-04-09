591400bfbe4612c387b22acb1dacc21b991b869d
Fayth Shepherd <faythshep@gmail.com>
2022-04-09 15:31:22 -0600

    Addition implementation working

--- calc.js
+++ calc.js
@@ -15,9 +15,24 @@ reader.question("What would you like to calculate?", function(input){
 	num2 = Number(tokens[2]);
 
 	console.log("mathsymbol:", mathSymbol)
-	//console.log("";)
+	console.log("Number 1:", num1)
+	console.log("Number 2:", num2)
 
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+  } else if (mathSymbol === "-"){
+		console.log(num1 - num2);
+  } else if (mathSymbol === "/"){
+	  	console.log(num1 / num2);
+  } else if (mathSymbol === "*"){
+	  	console.log(num1 * num2);
+  } else if (mathSymbol === "%"){
+	  	console.log(num1 % num2);
+  } else {
+	  console.log("Sorry, please try again")
+  }
 
+  	console.log
 	// This line closes the connection to the command line interface.
 	reader.close()
 
